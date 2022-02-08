85b2e83f6b971f78ac23870aec3dca240db158d5
Hestarus <deafendttack38@gmail.com>
2022-02-08 12:19:04 +0700

    Menambah line 5

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 print("ini commit ke 1")
 print("ini commit ke 2")
 print("ini commit ke 3")
-# print("ini commit ke 4") tidak ada karena sudah di reset  
+print("ini commit ke 4") #mengembalikan file dengan revert
+print("ini commit ke 5")
